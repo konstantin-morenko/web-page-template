ca3c9998e90833aaf9d4bd848852e51f5d5a06d8
Konstantin Morenko <me@konstantin-morenko.ru>
2020-02-14 09:36:26 +0300

    Fix

--- push.sh
+++ push.sh
@@ -4,16 +4,16 @@
 # https://gist.github.com/willprice/e07efd73fb7f13f917ea
 
 setup_git() {
-  git config --global user.email "actions@github.com"
-  git config --global user.name "Github.Actions"
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
 }
 
 commit_pdfs() {
-  git checkout --orphan "github"
+  git checkout --orphan "travis"
   git rm --cached $(git ls-files)
   git add -f index.html
   echo `git status`
-  git commit --message "Github action"
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
